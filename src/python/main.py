360606570e7f62bcbce2dd2b59d303688ace7485
Kiril Strezikozin <kirilstrezikozin@gmail.com>
2025-03-16 18:44:31 +0100

    src/python: add /healthz probe endpoint handler
    Signed-off-by: Kiril Strezikozin <kirilstrezikozin@gmail.com>

--- src/python/main.py
+++ src/python/main.py
@@ -1,7 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from fastapi.responses import PlainTextResponse
 
 app = FastAPI()
+
+
+@app.get("/healthz")
+async def healthz():
+    return Response(status_code=200)
 
 
 @app.get("/", response_class=PlainTextResponse)
