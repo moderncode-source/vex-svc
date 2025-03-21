14fa20d3671a5b637ad0d4e27f7458d6ffe37f3d
Kiril Strezikozin <kirilstrezikozin@gmail.com>
2025-03-21 17:50:45 +0100

    main.go,cmd/*: add root CLI cmd to start Vex service, add version cmd
    Signed-off-by: Kiril Strezikozin <kirilstrezikozin@gmail.com>

---
+++ main.go
@@ -0,0 +1,26 @@
+// SPDX-FileCopyrightText: 2025 The Vex Authors.
+//
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. You may not use this file except in compliance with the
+// terms of those licenses.
+
+// Package main is an entry point to the Vex service CLI.
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/moderncode-source/vex-svc/cmd"
+)
+
+func main() {
+	if err := cmd.Execute(); err != nil {
+		fmt.Printf("Vex CLI exited with error: %s\n", err)
+		os.Exit(1)
+	}
+}
