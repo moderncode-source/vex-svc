9f0cdc41a471daf0996b798d8279114886a00be9
Kiril Strezikozin <kirilstrezikozin@gmail.com>
2025-03-22 15:46:18 +0100

    vex/handlers,vex/service: change req handlers to be Service member-funcs
    Signed-off-by: Kiril Strezikozin <kirilstrezikozin@gmail.com>

--- vex/handlers.go
+++ vex/handlers.go
@@ -8,7 +8,7 @@
 // option. You may not use this file except in compliance with the
 // terms of those licenses.
 
-// Vex HTTP request handlers.
+// Vex service HTTP request handlers.
 
 package vex
 
@@ -23,7 +23,7 @@ import (
 
 // HealthHandler handles requests to service liveness probe endpoint that can
 // be used to check whether the server is running.
-func HealthHandler(w http.ResponseWriter, req *http.Request) {
+func (svc *Service) HealthHandler(w http.ResponseWriter, req *http.Request) {
 	h := w.Header()
 	h.Set("Content-Type", "application/json; charset=utf-8")
 	h.Set("X-Content-Type-Options", "nosniff")
@@ -40,7 +40,7 @@ func HealthHandler(w http.ResponseWriter, req *http.Request) {
 
 // ReadyHandler handles requests to service readiness probe endpoint that can
 // be used to check whether the server is ready to receive traffic.
-func ReadyHandler(w http.ResponseWriter, req *http.Request) {
+func (svc *Service) ReadyHandler(w http.ResponseWriter, req *http.Request) {
 	h := w.Header()
 	h.Set("Content-Type", "application/json; charset=utf-8")
 	h.Set("X-Content-Type-Options", "nosniff")
@@ -57,7 +57,7 @@ func ReadyHandler(w http.ResponseWriter, req *http.Request) {
 
 // PostQueueHandler handles requests that
 // post a new item into the submission queue.
-func PostQueueHandler(w http.ResponseWriter, req *http.Request) {
+func (svc *Service) PostQueueHandler(w http.ResponseWriter, req *http.Request) {
 	h := w.Header()
 	h.Set("Content-Type", "application/json; charset=utf-8")
 	h.Set("X-Content-Type-Options", "nosniff")
@@ -79,7 +79,7 @@ func PostQueueHandler(w http.ResponseWriter, req *http.Request) {
 	var submission Submission
 	decoder := json.NewDecoder(req.Body)
 	if err := decoder.Decode(&submission); err != nil {
-        // TODO: consider responding with the decoding error message here.
+		// TODO: consider responding with the decoding error message here.
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
@@ -91,7 +91,7 @@ func PostQueueHandler(w http.ResponseWriter, req *http.Request) {
 
 // GetQueueHandler handles requests to the submission queue
 // endpoint to retrieve information about the queue.
-func GetQueueHandler(w http.ResponseWriter, req *http.Request) {
+func (svc *Service) GetQueueHandler(w http.ResponseWriter, req *http.Request) {
 	h := w.Header()
 	h.Set("Content-Type", "application/json; charset=utf-8")
 	h.Set("X-Content-Type-Options", "nosniff")
