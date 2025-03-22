6731318e07755486c151c43efcbc346dcaf9d85c
Kiril Strezikozin <kirilstrezikozin@gmail.com>
2025-03-22 12:45:01 +0100

    vex/service: add zerolog logger to service, accept in New*
    Signed-off-by: Kiril Strezikozin <kirilstrezikozin@gmail.com>

--- vex/go.mod
+++ vex/go.mod
@@ -4,5 +4,12 @@ go 1.23.3
 
 require (
 	github.com/goccy/go-json v0.10.5
+	github.com/rs/zerolog v1.34.0
 	golang.org/x/net v0.37.0
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	golang.org/x/sys v0.31.0 // indirect
 )
--- vex/service.go
+++ vex/service.go
@@ -19,6 +19,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/rs/zerolog"
 	"golang.org/x/net/netutil"
 )
 
@@ -71,6 +72,7 @@ func (err *ServiceErr) Error() string {
 // Use [New] to create a new valid service instance.
 type Service struct {
 	server *http.Server
+	logger *zerolog.Logger
 }
 
 // New allocates and returns a new [Service] with [http.Server] that will
@@ -80,13 +82,14 @@ type Service struct {
 //
 // To choose your own handler or fall back to [http.DefaultServeMux],
 // use [NewWithHandler].
-func New(addr string) *Service {
+func New(addr string, logger *zerolog.Logger) *Service {
 	return &Service{
 		server: &http.Server{
 			ReadHeaderTimeout: serverReadHeaderTimeout,
 			Addr:              addr,
 			Handler:           ServiceMux,
 		},
+		logger: logger,
 	}
 }
 
@@ -97,13 +100,14 @@ func New(addr string) *Service {
 // If handler is nil, [http.DefaultServeMux] will be used.
 //
 // See also: [New].
-func NewWithHandler(addr string, handler http.Handler) *Service {
+func NewWithHandler(addr string, handler http.Handler, logger *zerolog.Logger) *Service {
 	return &Service{
 		server: &http.Server{
 			ReadHeaderTimeout: serverReadHeaderTimeout,
 			Addr:              addr,
 			Handler:           handler,
 		},
+		logger: logger,
 	}
 }
 
