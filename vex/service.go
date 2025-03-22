7e7ad422fac8151c7c1095589dac04f45f09621b
Kiril Strezikozin <kirilstrezikozin@gmail.com>
2025-03-22 19:14:36 +0100

    vex/service: add info msg on service Start call
    Signed-off-by: Kiril Strezikozin <kirilstrezikozin@gmail.com>

--- vex/service.go
+++ vex/service.go
@@ -184,6 +184,8 @@ func (svc *Service) Start() error {
 	// Limit the number of concurrent connections to the service.
 	ln := netutil.LimitListener(l, serverMaxConnections)
 
+	svc.logger.Info().Msgf("Vex service running on %s", l.Addr())
+
 	err = svc.server.Serve(ln)
 	if err != nil {
 		return &ServiceErr{Msg: "serve exited with error", Err: err}
